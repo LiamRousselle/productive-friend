64007116e742e65ac6715fc140211775d484e3a9
Liam <rousselle.mail@gmail.com>
2024-08-11 00:13:00 -0600

    Program.cs now exists.

---
+++ ProductiveFriend/src/Main/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductiveFriend.Program {
+    public class Program {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Main() {
+            
+            
+            Console.WriteLine("Starting productive friend");
+        }
+    }
+}
